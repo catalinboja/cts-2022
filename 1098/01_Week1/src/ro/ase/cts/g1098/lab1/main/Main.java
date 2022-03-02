d2d292b6b7d927e3ce8933334eacef02999d5cc0
Catalin <boja.catalin@hotmail.com>
2022-03-02 07:32:19 +0200

    Week 1

---
+++ 1098/01_Week1/src/ro/ase/cts/g1098/lab1/main/Main.java
@@ -0,0 +1,35 @@
+package ro.ase.cts.g1098.lab1.main;
+
+
+import ro.ase.cts.g1098.lab1.main.exceptions.InsufficientFundsException;
+import ro.ase.cts.g1098.lab1.main.interfaces.Profitable;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//syso for Eclipse
+		//sout for IntelliJ
+		
+		System.out.println("Hello. 1st Laboratory");
+		
+		//use the interface as a reference type
+		Profitable reference;
+		
+		try {
+			throw new InsufficientFundsException("You need a top-up");
+//			throw new InsufficientFundsException();
+		}
+		catch(InsufficientFundsException ex) {
+			System.out.println(ex.getMessage());
+		}
+		catch(Exception ex) {
+			System.out.println("We have a problem");
+		}
+		finally {
+			System.out.println("Exceptions example");
+		}
+		
+	}
+
+}
